0f497bbbc9814022aa38f529fb6f4cb95437dde9
CindySchalit <cindyschalit@gmail.com>
2018-01-04 18:10:06 -0800

    Feat(server): use separate server for database

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const _ = require('lodash')
+const axios = require('axios')
 
 // deconstruct
 const {
@@ -8,11 +8,6 @@ const {
   GraphQLInt,
   GraphQLSchema, // takes in a root query and returns a GraphQL schema
 } = graphql
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 }
-]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -34,7 +29,8 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       // go into data and look for what we want
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data)
       }
     }
   }
